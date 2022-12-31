fb9155653d7005a5ef5275680e7ffc69b67ed729
get.dipen@gmail.com <get.dipen@gmail.com>
2022-12-30 18:05:53 -0800

    added bootstrap and fontawesome

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Enable Inline Source Maps
+  config.sass.inline_source_maps = true
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,13 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+# From "bootstrap" gem
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "@popperjs/core", to: "popper.js", preload: true
+
+# Use all.js instead of fontawesome.js
+pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.2.1/js/all.js"
+
+# Custom JS
+pin "my_script", to: "my_script.js", preload: true
