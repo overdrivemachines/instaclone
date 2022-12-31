e12831f3ac26b6a875444f457a0ee3eef0b50bd8
get.dipen@gmail.com <get.dipen@gmail.com>
2022-12-30 18:46:49 -0800

    added home controller

--- app/javascript/my_script.js
+++ app/javascript/my_script.js
@@ -1,2 +1,5 @@
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
-const popoverList = [...popoverTriggerList].map((popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl));
+// This function runs on every page "load"
+document.addEventListener("turbo:load", () => {
+  const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+  const popoverList = [...popoverTriggerList].map((popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl));
+});
