004ad770cb3b934e3f980fc54438cbc43586e0aa
get.dipen@gmail.com <get.dipen@gmail.com>
2023-01-03 21:58:53 -0800

    added posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,6 +41,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable, :omniauthable
 
+  has_many :posts, dependent: :destroy
   has_one_attached :avatar
   has_one_attached :avatar_background
 
