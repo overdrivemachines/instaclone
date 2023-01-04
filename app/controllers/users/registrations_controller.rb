f993335be4ea3ef0d382dbd976d51ce1d696180d
get.dipen@gmail.com <get.dipen@gmail.com>
2023-01-03 19:49:40 -0800

    added method for edit and save details

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -18,6 +18,33 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def edit
   #   super
   # end
+
+  def edit_details
+    @user = current_user
+  end
+
+  def save_details
+    redirect_to root_url unless user_signed_in?
+    @user = current_user
+    if @user.update(user_details_params)
+      redirect_to users_edit_details_path, notice: "Your account details have been saved."
+    else
+      # redirect_to users_edit_details_path, alert: "There has been an error."
+      render :edit_details, status: :unprocessable_entity
+    end
+  end
+
+  # TODO
+  def destroy_avatar
+    current_user.avatar.purge
+    redirect_to users_edit_details_path, status: :see_other, notice: "Removed avatar image"
+  end
+
+  # TODO
+  def destroy_avatar_background
+    current_user.avatar_background.purge
+    redirect_to users_edit_details_path, status: :see_other, notice: "Removed avatar background image"
+  end
 
   # PUT /resource
   # def update
@@ -38,17 +65,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :full_name])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :full_name])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
@@ -56,7 +83,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_inactive_sign_up_path_for(_resource)
+    # super(resource)
+    # Go to: /home/message
+    home_message_path
+  end
+
+  def user_details_params
+    params.require(:user).permit(:full_name, :username, :avatar, :bio, :avatar_background)
+  end
 end
