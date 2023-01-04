d511b7f5acf9c020b52cb6bcbf334638496972bd
get.dipen@gmail.com <get.dipen@gmail.com>
2023-01-03 18:01:57 -0800

    added validation and image variants

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,4 +43,49 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
   has_one_attached :avatar_background
+
+  before_save :strip_and_downcase_fields
+
+  # Validations
+  USERNAME_REGEX = /\w+/
+  RESERVED_USERNAMES = Rails.application.routes.routes.map do |route|
+    route.path.spec.to_s[%r{(?<=\A/)\w+}]
+  end.compact.uniq.freeze
+  FULL_NAME_REGEX = /\A[^0-9`!@#$%\^&*+_=]+\z/
+
+  validates :bio, length: { maximum: 140 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Email invalid" },
+                    uniqueness: { case_sensitive: false },
+                    length: { minimum: 4, maximum: 254 }
+  validates :full_name, presence: true, format: { with: FULL_NAME_REGEX, message: "Name invalid" }, length: 5..25
+  validates :username, presence: true, format: { with: USERNAME_REGEX, message: "Username invalid" }, length: 5..25,
+                       uniqueness: { case_sensitive: false }, exclusion: { in: RESERVED_USERNAMES }
+
+  def avatar_as_thumbnail
+    avatar.variant(resize_to_limit: [96, 96]).processed
+  end
+
+  def avatar_background_as_thumbnail
+    avatar_background.variant(resize_to_limit: [500, 500]).processed
+  end
+
+  def initialize(args = {})
+    args[:email].strip!.downcase! if args[:email]
+    args[:username].strip!.downcase! if args[:username]
+    args[:full_name].strip! if args[:full_name]
+    args[:bio].strip! if args[:bio]
+    super
+  end
+
+  def strip_and_downcase_fields
+    (email.strip! && email.downcase!) unless email.blank?
+    (username.strip! && username.downcase!) unless username.blank?
+    full_name.strip! unless full_name.blank?
+    bio.strip! unless bio.blank?
+  end
+
+  # Overriding to_param so the username (instead of id) is used for route path and URL helpers:
+  def to_param
+    username
+  end
 end
