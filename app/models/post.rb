80c0f04ca5bbce2a5941fc8f35418105907fb272
get.dipen@gmail.com <get.dipen@gmail.com>
2023-01-06 21:54:45 -0800

    added methods for resizing image and to  get image's width and height

--- app/models/post.rb
+++ app/models/post.rb
@@ -21,4 +21,24 @@ class Post < ApplicationRecord
   belongs_to :user
 
   has_one_attached :image
+
+  # resize image so that the width or the height are at least 293 pixels
+  # does not resize images smaller than 293 pixels
+  def image_as_medium_thumbnail
+    if image_width > image_height
+      image.variant(resize_to_limit: [nil, 293]).processed
+    else
+      image.variant(resize_to_limit: [293, nil]).processed
+    end
+  end
+
+  # get image's width
+  def image_width
+    image.metadata[:width]
+  end
+
+  # get image's height
+  def image_height
+    image.metadata[:height]
+  end
 end
