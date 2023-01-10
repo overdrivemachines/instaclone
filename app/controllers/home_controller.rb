5bb9c6c73ef1c7eb7ea275bd1e0dcc0d3880ea45
get.dipen@gmail.com <get.dipen@gmail.com>
2023-01-10 01:07:35 -0800

    fetching 10 posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
   # @route GET / (root)
   def index
+    # displaying just 10 posts
+    # TODO: load on scroll or paginate
+    @posts = Post.includes(:user).limit(10)
   end
 
   # @route GET /home/message (home_message)
