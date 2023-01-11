2de54fb738720738920ea4439826ef518b8b0223
get.dipen@gmail.com <get.dipen@gmail.com>
2023-01-10 16:37:18 -0800

    added before validation method to fix comment's parent_id

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -27,6 +27,26 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :parent, class_name: "Comment", optional: true
   has_many :replies, class_name: "Comment", foreign_key: :parent_id, dependent: :destroy
+  before_validation :set_correct_parent
 
   validates :body, presence: true
+
+  def set_correct_parent
+    # top level comment, there is no parent
+    return if parent_id.blank?
+
+    # Check if parent_id is valid
+    throw :abort unless Comment.exists?(parent_id)
+
+    # 2nd level comment, leave the parent as is
+    return if parent.parent_id.blank?
+
+    # Check if parent's parent_id is valid
+    throw :abort unless Comment.exists?(parent.parent_id)
+
+    # Convert 3rd level to 2nd level comment
+    self.parent_id = parent.parent_id
+
+    # TODO: verify that the comment's post id is the same as the parent's post id
+  end
 end
