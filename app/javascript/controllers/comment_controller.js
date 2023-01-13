e4565a3e9f66e40d0e9612f585d1dd751ea26d7f
get.dipen@gmail.com <get.dipen@gmail.com>
2023-01-12 21:27:12 -0800

    added comments

--- app/javascript/controllers/comment_controller.js
+++ app/javascript/controllers/comment_controller.js
@@ -1,15 +1,18 @@
 import { Controller } from "@hotwired/stimulus";
 
+// Used by comments/edit.html.erb
+// Resizes the height of the edit and new textareas to fit content
 export default class extends Controller {
   static targets = ["txtarea"];
+
+  // Called when comment edit form is generated
   connect() {
-    // Called when comment edit form is generated
     this.resizeTextArea();
   }
 
+  // Called when input changes in an edit or new textarea
   resizeTextArea() {
     // this.element is div.comment
-    // Called when text changes in textarea
     this.txtareaTarget.style.height = "auto";
     this.txtareaTarget.style.height = this.txtareaTarget.scrollHeight + "px";
   }
