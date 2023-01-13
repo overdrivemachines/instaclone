90133d08174fa4d48cee7b1e45739429d965f8f5
get.dipen@gmail.com <get.dipen@gmail.com>
2023-01-13 13:37:54 -0800

    create and destroy likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,22 @@
 class LikesController < ApplicationController
   # @route POST /likes (likes)
   def create
+    @like = current_user.likes.new(like_params)
+    @like.save
+    render partial: "button", locals: { likeable: @like.likeable }
+    # render turbo_stream: turbo_stream.replace()
   end
 
   # @route DELETE /likes/:id (like)
   def destroy
+    @like = current_user.likes.find(params[:id])
+    @like.destroy
+    render partial: "button", locals: { likeable: @like.likeable }
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:likeable_id, :likeable_type)
   end
 end
