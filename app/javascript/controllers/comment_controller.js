3c201b1ab4a00bd2487d91e99b42a97fe786fc60
get.dipen@gmail.com <get.dipen@gmail.com>
2023-01-13 18:35:04 -0800

    remove flash messages when text area is focused

--- app/javascript/controllers/comment_controller.js
+++ app/javascript/controllers/comment_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus";
 // Used by comments/edit.html.erb
 // Resizes the height of the edit and new textareas to fit content
 export default class extends Controller {
-  static targets = ["txtarea"];
+  static targets = ["txtarea", "commentForm"];
 
   // Called when comment edit form is generated
   connect() {
@@ -15,5 +15,18 @@ export default class extends Controller {
     // this.element is div.comment
     this.txtareaTarget.style.height = "auto";
     this.txtareaTarget.style.height = this.txtareaTarget.scrollHeight + "px";
+  }
+
+  // called when text area is focused
+  removeFlashMessages() {
+    const textSuccessEl = this.commentFormTarget.querySelector(".text-success");
+    const textDangerEl = this.commentFormTarget.querySelector(".text-danger");
+    // TODO: Instead of remove() add a css animate class to fade out
+    if (textSuccessEl) {
+      textSuccessEl.remove();
+    }
+    if (textDangerEl) {
+      textDangerEl.remove();
+    }
   }
 }
