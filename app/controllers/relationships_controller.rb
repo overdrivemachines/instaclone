2d4e668a89dbffe37eed72c008c994753a4bbacf
get.dipen@gmail.com <get.dipen@gmail.com>
2023-01-14 20:23:38 -0800

    added follow/unfollow actions

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,24 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
   # @route POST /relationships (relationships)
-  def create; end
+  def create
+    @relationship = Relationship.new(relationship_params)
+    @relationship.follower = current_user
+    @relationship.save
+    render partial: "button", locals: { followee: @relationship.followee }
+  end
 
   # @route DELETE /relationships/:id (relationship)
-  def destroy; end
+  def destroy
+    relationship = Relationship.find(params[:id])
+    followee = relationship.followee
+    relationship.destroy
+    render partial: "button", locals: { followee: followee }
+  end
+
+  private
+
+  def relationship_params
+    params.require(:relationship).permit(:followee_id)
+  end
 end
