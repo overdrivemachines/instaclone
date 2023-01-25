683aa066d1d3e56a49e423a47ac94d42ec8a27ab
get.dipen@gmail.com <get.dipen@gmail.com>
2023-01-24 23:57:33 -0800

    added helper functions for follow modal

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,11 @@
 module UserHelper
+  # used by users/follow.html.erb
+  def caption_for(follow)
+    follow == "followers" ? "followers" : "following"
+  end
+
+  # used by users/follow.html.erb
+  def message_zero_follows(follow)
+    follow == "followers" ? "No followers yet." : "Not following anyone."
+  end
 end
