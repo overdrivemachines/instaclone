e0a95ca491d1a0e9d268cbf5c869ccb141a578ac
get.dipen@gmail.com <get.dipen@gmail.com>
2023-01-24 23:58:42 -0800

    added follow action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,14 @@ class UsersController < ApplicationController
     @posts = @user.posts.all
   end
 
+  # @route GET /:username/followers (user_followers)
+  # @route GET /:username/followees (user_followees)
+  def follow
+    # @follow is either followers or followees
+    @follow = request.path.split("/").last
+    @follow_users = @user.send(@follow)
+  end
+
   private
 
   def set_user
