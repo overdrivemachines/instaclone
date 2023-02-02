d71b3a666605a6f1593ccb7caf3601588313de34
get.dipen@gmail.com <get.dipen@gmail.com>
2023-02-02 11:17:16 -0800

    trying turbo streams

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,6 +6,16 @@ class LikesController < ApplicationController
     @like.save
     render partial: "button", locals: { likeable: @like.likeable }
     # render turbo_stream: turbo_stream.replace()
+
+    # respond_to do |format|
+    #   format.turbo_stream do
+    #     render turbo_stream: [
+    #       turbo_stream.replace("like_post_#{@like.likeable_id}", partial: "button",
+    #                                                              locals: { likeable: @like.likeable }),
+    #       turbo_stream.replace("likes_count_post_#{@like.likeable_id}", @like.likeable.likes.size.to_s + " like(s)")
+    #     ]
+    #   end
+    # end
   end
 
   # @route DELETE /likes/:id (like)
