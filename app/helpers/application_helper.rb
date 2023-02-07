7b5e629c0debe3833fcec2cbf814c23b8152cded
get.dipen@gmail.com <get.dipen@gmail.com>
2023-02-07 15:04:18 -0800

    using helpers for avatar and avatar bg

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  # returns an image tag for user avatar image (thumbnail)
+  def user_avatar_image(user:, class_name: nil)
+    if user.avatar.attached?
+      image_tag user.avatar_as_thumbnail, class: class_name
+    else
+      image_tag "person-placeholder.jpg", class: class_name
+    end
+  end
+
+  # returns an image tag for user avatar background image (thumbnail)
+  def user_avatar_background_image(user:, class_name: nil)
+    if user.avatar_background.attached?
+      image_tag current_user.avatar_background_as_thumbnail, class: class_name
+    else
+      image_tag "bg-placeholder", class: class_name
+    end
+  end
 end
